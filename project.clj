57f79c2c3c6599af2b7c337c2598b737b04c8d9a
David Thomas Hume <dthume@gmail.com>
2009-12-06 02:37:33 +0000

    Initial commit

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject com.dthume/incanter-utils "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies
+  [[org.clojure/clojure "1.1.0-new-SNAPSHOT"]
+   [org.clojure/clojure-contrib "1.0-SNAPSHOT"]
+   [com.dthume/util "1.0.0-SNAPSHOT"]
+   [incanter "1.0-alpha"]])
---
+++ src/com/dthume/incanter_utils.clj
@@ -0,0 +1,18 @@
+(ns
+    com.dthume.incanter-utils
+  (:require [incanter.core :as incant]
+            [com.dthume.util.xrels :as xr]))
+
+(defn xrel->dataset
+  "Transform xrel to an incanter dataset"
+  [xrel]
+  (let [ks (into [] (xr/distinct-keys xrel))]
+    (incant/dataset ks
+                    (map (fn [row] (map row ks))
+                         xrel))))
+
+(defn dataset->xrel
+  "Transform ds, an incanter dataset to an xrel; note that since xrels are
+inherently sets, the resulting xrel may contain less rows than ds."
+  [ds]
+  (set (:rows ds)))
